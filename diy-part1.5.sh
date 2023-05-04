1c1c00190e647e121414b1aa5435be4eb234f195
m0eak <zhongliangpsp@sina.com>
2023-05-04 20:41:25 +0800

    Update diy-part1.5.sh

--- diy-part1.5.sh
+++ diy-part1.5.sh
@@ -1,4 +1,4 @@
 cd feeds/luci/themes
 rm -r luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon
+git clone --branch v1.7.3 https://github.com/jerrykuku/luci-theme-argon
 cd ../../.. && ./scripts/feeds update -a
